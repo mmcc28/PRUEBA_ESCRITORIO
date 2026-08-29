170eae6809d6bc01f658f1f672e5607fb020702b
mmcc28 <mirnamaria2801@gmail.com>
2026-08-29 16:07:52 -0600

    PRIMEROS MENSAJES DE RAMA
    HACIENDO PRUEBA 1

--- ProyectPrueba/src/proyectprueba/ProyectPrueba.java
+++ ProyectPrueba/src/proyectprueba/ProyectPrueba.java
@@ -14,7 +14,8 @@ public class ProyectPrueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+        // TODO code application logic here   
+        System.out.println("HOLA ESTOY EN LA RAMA DE MIRNA");        
+    }// FIN MAIN
+     
+}// FIN CLASS 
