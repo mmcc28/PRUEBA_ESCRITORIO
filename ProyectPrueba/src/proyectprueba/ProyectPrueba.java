8bc5d497c252f46c3800ccc66d3c14f716865989
mmcc28 <mirnamaria2801@gmail.com>
2026-08-29 16:14:27 -0600

    prueba 2
    prueba 2

--- ProyectPrueba/src/proyectprueba/ProyectPrueba.java
+++ ProyectPrueba/src/proyectprueba/ProyectPrueba.java
@@ -16,6 +16,9 @@ public class ProyectPrueba {
     public static void main(String[] args) {
         // TODO code application logic here   
         System.out.println("HOLA ESTOY EN LA RAMA DE MIRNA");        
+        
+        System.out.println("PRUEBA 2");
+        
     }// FIN MAIN
      
 }// FIN CLASS 
